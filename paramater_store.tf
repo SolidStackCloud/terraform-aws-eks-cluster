5b9976f374e2d9d14d6db046dc46bb8362fe5a9f
Lucas Gabriel <lucas@solidstackcloud.com>
2025-08-20 11:21:35 -0300

    add: parameter instance_profile

--- paramater_store.tf
+++ paramater_store.tf
@@ -3,3 +3,9 @@ resource "aws_ssm_parameter" "cluster_name" {
   name  = "/${var.project_name}/cluster-name"
   value = aws_eks_cluster.main.id
 }
+
+resource "aws_ssm_parameter" "instance_profile" {
+  type = "String"
+  name  = "/${var.project_name}/instance-profile"
+  value = aws_iam_instance_profile.workers_nodes.arn
+}
